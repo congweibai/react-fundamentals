4dcb07a84a944869b2c40c14f9891a27b50745cb
Wyatt Bai <wyatt.bai@caremappr.com.au>
2024-10-17 21:08:23 +1030

    feat: size props

--- exercises/06.styling/03.problem.size-prop/index.tsx
+++ exercises/06.styling/03.problem.size-prop/index.tsx
@@ -3,18 +3,19 @@ import { createRoot } from 'react-dom/client'
 export function Box({
 	// 💯 you can keep the style and className props here, but you can make this
 	// still work if you remove them. Give that a shot if you want.
-	style = {},
-	className = '',
+	size,
+	style,
 	// 🐨 add a size prop here
 	...otherProps // 🦺 intersect (&) this with an object that has a size prop type here which is
 	// optional and is one of "small", "medium", or "large"
-}: React.ComponentProps<'div'>) {
+}: React.ComponentProps<'div'> & { size: 'small' | 'medium' | 'large' }) {
 	// 🐨 based on the size prop, define a new variable called sizeClassName
+	const sizeClassName = `box--${size}`
 	return (
 		<div
 			// 🐨 add the sizeClassName to the className prop
 			// 💯 for something extra, handle the case where no className is given (remove extra spaces).
-			className={`box ${className}`}
+			className={`box ${sizeClassName}`}
 			style={{ fontStyle: 'italic', ...style }}
 			{...otherProps}
 		/>
@@ -25,13 +26,13 @@ function App() {
 	return (
 		<div>
 			{/* 🐨 update all these boxes to use the size prop */}
-			<Box className="box--small" style={{ backgroundColor: 'lightblue' }}>
+			<Box size="small" style={{ backgroundColor: 'lightblue' }}>
 				small lightblue box
 			</Box>
-			<Box className="box--medium" style={{ backgroundColor: 'pink' }}>
+			<Box size="medium" style={{ backgroundColor: 'pink' }}>
 				medium pink box
 			</Box>
-			<Box className="box--large" style={{ backgroundColor: 'orange' }}>
+			<Box size="large" style={{ backgroundColor: 'orange' }}>
 				large orange box
 			</Box>
 			<Box>sizeless colorless box</Box>
