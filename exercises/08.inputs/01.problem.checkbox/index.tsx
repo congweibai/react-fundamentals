8397fcbb387a3c02f0d09723e979e6ced3f8b92b
Wyatt Bai <wyatt.bai@caremappr.com.au>
2024-10-18 16:56:39 +1030

    feat: input checkboxs

--- exercises/08.inputs/01.problem.checkbox/index.tsx
+++ exercises/08.inputs/01.problem.checkbox/index.tsx
@@ -28,6 +28,10 @@ function App() {
 			</div>
 			{/* 🐨 add a checkbox named "waiver" with the label "Waiver Signed" */}
 			{/* 💰 put the <input> inside the <label> */}
+			<label>
+				<input name="waiver" type="checkbox" />
+				Waiver Signed
+			</label>
 			<div>
 				<label htmlFor="startDateInput">Start Date:</label>
 				<input id="startDateInput" name="startDate" type="date" />
